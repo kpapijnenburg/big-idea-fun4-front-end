785cb7596d908c4dd018ba8096a5d43223a68d34
KpaPijnenburg <kpapijnenburg@gmail.com>
2019-04-17 11:06:33 +0200

    Added basic packages

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <mdc-top-app-bar title="App" event="nav"></mdc-top-app-bar>
+</template>
+
+<script>
+
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="stylus">
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMDCAdapter from 'vue-mdc-adapter'
+
+Vue.use(VueMDCAdapter)
 
 Vue.config.productionTip = false
 
